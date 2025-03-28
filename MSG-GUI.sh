24ad50f0b91217968d89d32178ccf5439de7f11d
BL4K3 <blake.staniland@spcc.nsw.edu.au>
2025-03-28 18:09:06 +1100

    Fix If statement

--- MSG-GUI.sh
+++ MSG-GUI.sh
@@ -1,8 +1,8 @@
 # request server IP and Port
-S_IP=`osascript -e 'text returned of (display dialog "Server IP:" default answer "")'`
-S_Port=`osascript -e 'text returned of (display dialog "Server Port:" default answer "")'`
-pt1=`osascript -e 'text returned of (display dialog "Receiver Port:" default answer "")'`
-Uname=`osascript -e 'text returned of (display dialog "Username:" default answer "")'`
+S_IP=`osascript -e 'text returned of (display dialog "Server IP:" default answer "192.168.1.7")'`
+S_Port=`osascript -e 'text returned of (display dialog "Server Port:" default answer "8888")'`
+pt1=`osascript -e 'text returned of (display dialog "Receiver Port:" default answer "8889")'`
+Uname=`osascript -e 'text returned of (display dialog "Username:" default answer "Blake")'`
 # send this computers ip to the server for registration
 ips=`ifconfig | grep "inet " | grep -v 127.0.0.1 | awk '{print $2}'`
 echo "$ips" | nc $S_IP $S_Port
@@ -10,15 +10,15 @@ echo "$ips" | nc $S_IP $S_Port
 receive() {
 while true; do
 Rx=`nc -l ${pt1}`
-if ![$Rx==""]; then
 echo "Msg Rx: $Rx"
-fi
+printf "Message: "
 done
 }
 send() {
 local msg="$1"
 echo "<${Uname}> $msg" | nc $S_IP $S_Port
 }
+printf "Message: "
 while true; do
 receive &
 read -p "Message: " to_send
