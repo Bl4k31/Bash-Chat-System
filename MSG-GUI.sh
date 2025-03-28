40a642c21c5ba2dc279113ab8aa39b1618d27df9
BL4K3 <blake.staniland@spcc.nsw.edu.au>
2025-03-28 18:47:06 +1100

    Trying to prevent recursive nc -l commands

--- MSG-GUI.sh
+++ MSG-GUI.sh
@@ -1,7 +1,7 @@
 # request server IP and Port
 S_IP=`osascript -e 'text returned of (display dialog "Server IP:" default answer "192.168.1.7")'`
 S_Port=`osascript -e 'text returned of (display dialog "Server Port:" default answer "8888")'`
-pt1=`osascript -e 'text returned of (display dialog "Receiver Port:" default answer "8889")'`
+pt1=`osascript -e 'text returned of (display dialog "Receiver Port:" default answer "8890")'`
 Uname=`osascript -e 'text returned of (display dialog "Username:" default answer "Blake")'`
 # send this computers ip to the server for registration
 ips=`ifconfig | grep "inet " | grep -v 127.0.0.1 | awk '{print $2}'`
@@ -9,18 +9,17 @@ echo "$ips" | nc $S_IP $S_Port
 
 receive() {
 while true; do
-Rx=`nc -l ${pt1}`
-echo "$Rx"
-printf "Message: "
+Rx=$(nc -l -p "${pt1}" | head -n 1)
+osascript -e display dialog "New message: ${Rx}" buttons {"OK"}
 done
+
 }
 send() {
 local msg="$1"
-echo "<${Uname}> $msg" | nc $S_IP $S_Port
+echo "<${Uname}> ${msg}" | nc $S_IP $S_Port
 }
-printf "Message: "
 while true; do
 receive &
 read to_send
-send $to_send
+send "${to_send}"
 done
