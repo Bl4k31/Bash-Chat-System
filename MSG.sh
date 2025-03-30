75b4edb37de50d807dc4e8e03a91af3d71d4de5f
BL4K3 <blake.staniland@spcc.nsw.edu.au>
2025-03-31 08:57:07 +1100

    Added some GUI to MSG.sh

--- MSG.sh
+++ MSG.sh
@@ -1,8 +1,8 @@
 # request server IP and Port
-read -p "Server IP: " S_IP
-read -p "Server Port: " S_Port
-read -p "Receiver Port: " pt1
-read -p "Username: " Uname
+S_IP=`osascript -e 'text returned of (display dialog "Server IP:" default answer "192.168.1.7")'`
+S_Port=`osascript -e 'text returned of (display dialog "Server Port:" default answer "8888")'`
+pt1=`osascript -e 'text returned of (display dialog "Receiver Port:" default answer "8890")'`
+Uname=`osascript -e 'text returned of (display dialog "Username:" default answer "Blake")'`
 # send this computers ip to the server for registration
 ips=`ifconfig | grep "inet " | grep -v 127.0.0.1 | awk '{print $2}'`
 echo "$ips" | nc $S_IP $S_Port
