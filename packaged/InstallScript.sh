0477ca7a8c374f4c6663a45caf843cb4ca2d21d0
BL4K3 <blake.staniland@spcc.nsw.edu.au>
2025-03-31 09:39:59 +1100

    InstallScript Unix Safe

--- packaged/InstallScript.sh
+++ packaged/InstallScript.sh
@@ -34,20 +34,22 @@ printf "\e[34;1mUpdating Server permissions in ${dtop}/Server.sh\e[0m\n"
 chmod u+x "${dtop}/Server.sh"
 sleep 0.5
 }
-ins_oldserver() {
-printf "\e[34;1mCopying Server to ${dtop}\e[0m\n"
-cp "${ins_loc}/UnixSafe/Server-NonGUI.sh" ${dtop}
-sleep 0.5
-printf "\e[34;1mUpdating Server permissions in ${dtop}/Server.sh\e[0m\n"
-chmod u+x "${dtop}/Server.sh"
-sleep 0.5
-}
 ins_oldfiles() {
-printf "\e[34;1mCopying Messenger to ${dtop}\e[0m\n"
-cp "${ins_loc}/UnixSafe/MSG-NonGUI.sh" ${dtop}
+printf "\e[34;1mCopying Message Receiver to ${dtop}\e[0m\n"
+cp "${ins_loc}/UnixSafe/RxMSG.sh" ${dtop}
 sleep 0.5
-printf "\e[34;1mUpdating Messenger permissions in ${dtop}/MSG.sh\e[0m\n"
-chmod u+x "${dtop}/MSG.sh"
+printf "\e[31;1mClearing Malware warning for ${dtop}/RxMSG.sh (May Error)\e[0m\n"
+xattr -d com.apple.quarantine "${dtop}/RxMSG.sh"
+printf "\e[34;1mUpdating Message Receiver permissions in ${dtop}/RxMSG.sh\e[0m\n"
+chmod u+x "${dtop}/RxMSG.sh"
+sleep 0.5
+printf "\e[34;1mCopying Message Sender to ${dtop}\e[0m\n"
+cp "${ins_loc}/UnixSafe/TxMSG.sh" ${dtop}
+sleep 0.5
+printf "\e[31;1mClearing Malware warning for ${dtop}/TxMSG.sh (May Error)\e[0m\n"
+xattr -d com.apple.quarantine "${dtop}/TxMSG.sh"
+printf "\e[34;1mUpdating Message Sender permissions in ${dtop}/TxMSG.sh\e[0m\n"
+chmod u+x "${dtop}/TxMSG.sh"
 sleep 0.5
 }
 ins_newfiles() {
@@ -61,18 +63,14 @@ chmod u+x "${dtop}/MSG.sh"
 sleep 0.5
 }
 if confirm "Above details correct?"; then
-
-if confirm "Do you want to install the UNIX Compatable version?"; then
-ins_oldfiles
-if confirm "Do you also want to install the server?"; then
-ins_oldserver
-fi
-fi
-if confirm "Do you want to install the Macos only GUI Ver?"; then
-ins_newfiles
 if confirm "Do you also want to install the server?"; then
 ins_server
 fi
+if confirm "Do you want to install the UNIX Compatable version?"; then
+ins_oldfiles
+fi
+if confirm "Do you want to install the Macos only GUI Ver?"; then
+ins_newfiles
 fi
 # ins_updater
 printf "\e[32;1mInstallation Successful!\e[0m\n"
