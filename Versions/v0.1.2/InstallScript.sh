2568e0d0272d2b4804ac41412ff95257ada273e7
BL4K3 <blake.staniland@spcc.nsw.edu.au>
2025-03-31 12:45:49 +1100

    Packaged v0.1.2

---
+++ Versions/v0.1.2/InstallScript.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+printf "\e[34;1mMessage system Self-install script\e[0m\n"
+ins_loc=`pwd`
+usr_dir=`whoami`
+read -p "Set Install Location (From /Users.${usr_dir}/ (Optional; leave blank): " dtop
+if [ "$dtop" == "" ] || [ "$dtop" == " " ]; then
+dtop="/Users/${usr_dir}"
+else
+dtop="Users/${usr_dir}/${dtop}"
+fi
+printf "\e[32;1mFolder Location: ${ins_loc}\e[0m\n"
+printf "\e[33;1mInstall Folder: ${dtop}\e[0m\n"
+printf "\e[30;1mUsername: ${usr_dir}\e[0m\n"
+confirm() {
+  local prompt="$1"
+  local response
+
+  while true; do
+    read -p "$prompt (y/n): " response
+    case "$response" in
+      [yY]*) return 0 ;; # Yes
+      [nN]*) return 1 ;; # No
+      *) echo "Please answer y or n." ;;
+    esac
+  done
+}
+ins_server() {
+printf "\e[34;1mCopying Server to ${dtop}\e[0m\n"
+cp "${ins_loc}/Server.sh" ${dtop}
+sleep 0.5
+printf "\e[31;1mClearing Malware warning for ${dtop}/Server.sh (May Error)\e[0m\n"
+xattr -d com.apple.quarantine "${dtop}/Server.sh"
+printf "\e[34;1mUpdating Server permissions in ${dtop}/Server.sh\e[0m\n"
+chmod u+x "${dtop}/Server.sh"
+sleep 0.5
+}
+ins_nixserver() {
+printf "\e[34;1mCopying Server to ${dtop}\e[0m\n"
+cp "${ins_loc}/UnixSafe/Server-NonGUI.sh" ${dtop}
+sleep 0.5
+printf "\e[34;1mUpdating Server permissions in ${dtop}/Server-NonGUI.sh\e[0m\n"
+chmod u+x "${dtop}/Server-NonGUI.sh"
+sleep 0.5
+}
+ins_nixfiles() {
+printf "\e[34;1mCopying Messenger to ${dtop}\e[0m\n"
+cp "${ins_loc}/UnixSafe/MSG-NonGUI.sh" ${dtop}
+sleep 0.5
+printf "\e[34;1mUpdating Messenger permissions in ${dtop}/MSG-NonGUI.sh\e[0m\n"
+chmod u+x "${dtop}/MSG-NonGUI.sh"
+sleep 0.5
+}
+ins_newfiles() {
+printf "\e[34;1mCopying Messenger to ${dtop}\e[0m\n"
+cp "${ins_loc}/MSG.sh" ${dtop}
+sleep 0.5
+printf "\e[31;1mClearing Malware warning for ${dtop}/MSG.sh (May Error)\e[0m\n"
+xattr -d com.apple.quarantine "${dtop}/MSG.sh"
+printf "\e[34;1mUpdating Messenger permissions in ${dtop}/MSG.sh\e[0m\n"
+chmod u+x "${dtop}/MSG.sh"
+sleep 0.5
+}
+if confirm "Above details correct?"; then
+
+if confirm "Do you want to install the UNIX Compatable version (Recommended)?"; then
+ins_nixfiles
+if confirm "Do you also want to install the server?"; then
+ins_nixserver
+fi
+fi
+if confirm "Do you want to install the Macos only GUI Ver (Broken -don't use)?"; then
+ins_newfiles
+if confirm "Do you also want to install the server?"; then
+ins_server
+fi
+fi
+# ins_updater
+printf "\e[32;1mInstallation Successful!\e[0m\n"
+fi
---
+++ Versions/v0.1.2/README.md
@@ -0,0 +1,31 @@
+a Quick and dirty Bash Messaging System I'm Developing for fun.
+
+_Trying to make a GUI Version (Applescript) - Might take a while_
+
+
+# INITIAL SETUP:
+## Installing & Updating (Should work for all major NIX' Systems)
+### Clone repo:
+**_In terminal:_** 
+`git clone https://github.com/Bl4k31/MSGSYS; cd ./MSGSYS; sh ./InstallScript.sh`
+### OR
+**_In terminal:_** 
+`curl -L -O https://github.com/Bl4k31/MSGSYS/archive/v0.1.1.zip; unzip v0.1.1.zip; cd ./MSGSYS-0.1.1; sh ./InstallScript.sh`
+## Changing Default app for .sh files (Macos Only)
+1. Select `Get Info`
+2. Select the `Open With:` Dropdown
+3. Click on the `(application that opens .sh)` navigate to `Other…`
+
+	a. Change `Enable: #########` to `Enable: All applications`.\
+	b. Search for, and click: `Terminal`.\
+	c. Select `ADD`.
+4. Click `Change All…`
+
+To Open chat go to your user folder (Macintosh HD/Users/firstname.lastname)
+Double click the Tx and Rx scripts
+
+# Message System connection
+### MSG.sh:
+Enter the server IP, Port, and Reciever Port (Provided by the host) into the prompts
+### MSG-NonGUI.sh
+Enter the server IP, Port, and Reciever Port (Provided by the host) into the terminal
